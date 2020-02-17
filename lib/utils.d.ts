8bc217ac4dd1daecd87cad497790c64e00abf56c
Rui Ying <yingrui205@gmail.com>
2020-02-17 17:28:15 +0800

    v2.1.2

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -2,3 +2,5 @@ import { SemesterType } from "./types";
 export declare function parseSemesterType(n: number): SemesterType;
 export declare function trimAndDefine(text: string | undefined | null): string | undefined;
 export declare function mapGradeToLevel(grade: number | null): string | undefined;
+export declare const JSONP_EXTRACTOR_NAME = "thu_learn_lib_jsonp_extractor";
+export declare function extractJSONPResult(jsonp: string): any;
